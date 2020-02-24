ab62e5f23ef7ee18aebbdb03e48730a2e2d58ac2
1838760 <1838760@20.8.20.110>
2020-02-24 12:02:58 -0800

    Product class

--- AmazonProject/src/Product.java
+++ AmazonProject/src/Product.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
 
 public abstract class Product {
-
+	private String name;
+	private double price;
+	private double rating;
+	cartItems = new String[]{""};
+	
 	public String getName() {
 		return name;		
 	}
@@ -13,13 +18,14 @@ public abstract class Product {
 		return rating;
 	}
 	
-	public void addCart() {
-		
+	public void addCart(String item) {
+		ArrayList<String> cartItems = new ArrayList<String>();
+		cartItems.add(item);
 	}
 	
 	public void checkOut() {
-		
+		System.out.println("Purchase all items in cart?");
+		System.out.println(cartItems);
 	}
-
 
 }
