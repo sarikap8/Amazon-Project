309e50a4a65ddaaa2190552088e406d898f08920
1838760 <1838760@20.8.20.110>
2020-02-28 12:20:36 -0800

    Fixed UI, cart button

--- AmazonProject/src/ClothingUI.java
+++ AmazonProject/src/ClothingUI.java
@@ -47,15 +47,6 @@ public class ClothingUI extends JFrame {
 			}
 		});
 	}
-	static JLabel lblRating = new JLabel("Rating: ");
-	static JLabel lblProductPhoto = new JLabel("Product Photo ");
-	static JLabel lblProductName = new JLabel(" NAME: ");
-	static JLabel lblAttributes = new JLabel("");
-
-
-	public ClothingUI() {
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 831, 551);
 
 	/**
 	 * Create the frame.
@@ -65,6 +56,11 @@ public class ClothingUI extends JFrame {
 	static JLabel lblProductName = new JLabel(" NAME: ");
 	static JLabel lblAttributes = new JLabel("");
 
+	
+	public ClothingUI() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 831, 551);
+
 		Container content = getContentPane();
 		content.setBackground(new Color(204, 204, 204));
 		getContentPane().setLayout(null);
@@ -73,13 +69,13 @@ public class ClothingUI extends JFrame {
 		menuBar.setBounds(0, 0, 807, 30);
 		getContentPane().add(menuBar);
 
-		JMenuItem menuItemFiction = new JMenuItem("Sweatshirts");
+		JMenuItem menuItemFiction = new JMenuItem("Shirts");
 		menuBar.add(menuItemFiction);
 
-		JMenuItem menuItemNonfiction = new JMenuItem("Shirts");
+		JMenuItem menuItemNonfiction = new JMenuItem("Jeans");
 		menuBar.add(menuItemNonfiction);
 
-		JMenuItem menuItemScifi = new JMenuItem("Jeans");
+		JMenuItem menuItemScifi = new JMenuItem("Sweatshirts");
 		menuBar.add(menuItemScifi);
 
 		lblProductPhoto.setBounds(6, 77, 264, 334);
@@ -105,6 +101,7 @@ public class ClothingUI extends JFrame {
 		JButton button = new JButton("<");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				 
 			}
 		});
 		button.setBounds(0, 423, 75, 29);
@@ -113,6 +110,7 @@ public class ClothingUI extends JFrame {
 		JButton button_1 = new JButton(">");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 			}
 		});
 		button_1.setBounds(209, 423, 75, 29);
@@ -126,99 +124,6 @@ public class ClothingUI extends JFrame {
 				JButton btnAddtoCart = new JButton("Add to cart");
 				btnAddtoCart.setBounds(671, 423, 136, 41);
 				getContentPane().add(btnAddtoCart);
-	public static void updateUI() {
-		Books b = AmazonUI.productStorage.getBook(bookIndex);
-		lblAttributes.setText("<html> Author: " + b.getAuthorName() + "<br> </br> Publisher: " + b.getPublisherName()
-				+ "<br> </br> Page Count: " + b.getPageCount() + "<br> </br>");
-		lblProductPhoto
-		.setIcon(new ImageIcon("src/" + AmazonUI.productStorage.getBook(bookIndex).getPhotoName() + ".jpg"));
-		lblRating.setText("RATING: " + AmazonUI.productStorage.getBook(bookIndex).getRating());
-		lblProductName.setText("NAME: " + AmazonUI.productStorage.getBook(bookIndex).getName()); 
-
-	/**
-	 * Create the frame.
-	 */ 
-
-	
-
-	public ClothingUI() {
-		bookIndex = 0;
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 831, 551);
-
-		Container content = getContentPane();
-		content.setBackground(new Color(204, 204, 204));
-		getContentPane().setLayout(null);
-
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 807, 30);
-		getContentPane().add(menuBar);
-
-		JMenuItem menuItemFiction = new JMenuItem("Shirts");
-		menuBar.add(menuItemFiction);
-
-		JMenuItem menuItemNonfiction = new JMenuItem("Jeans");
-		menuBar.add(menuItemNonfiction);
-
-		JMenuItem menuItemScifi = new JMenuItem("Sweaters");
-		menuBar.add(menuItemScifi);
-
-		JButton btnAddtoCart = new JButton("Add to cart");
-		menuBar.add(btnAddtoCart);
-
-		lblProductPhoto.setBounds(6, 77, 145, 174);
-		
-		getContentPane().add(lblProductPhoto);
-		
-		lblProductName.setForeground(new Color(255, 255, 255));
-		lblProductName.setBackground(new Color(102, 0, 0));
-		lblProductName.setOpaque(true);
-		lblProductName.setBounds(10, 42, 797, 23);
-		getContentPane().add(lblProductName);
-
-		lblRating.setBackground(new Color(153, 102, 102));
-		lblRating.setOpaque(true);
-		lblRating.setBounds(163, 67, 272, 30);
-		getContentPane().add(lblRating);
-
-		lblAttributes.setOpaque(true);
-		lblAttributes.setBackground(new Color(204, 153, 153));
-		lblAttributes.setBounds(163, 104, 272, 127);
-		getContentPane().add(lblAttributes);
-
-		JButton button = new JButton("<");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bookIndex --; 
-				if(bookIndex < 0) {
-					bookIndex = 2; 
-				}
-				updateUI(); 
-			}
-		});
-		button.setBounds(157, 243, 75, 29);
-		getContentPane().add(button);
-
-		JButton button_1 = new JButton(">");
-		button_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bookIndex++; 
-				if (bookIndex > 2) {
-					bookIndex = 0; 
-				}
-				//Books c = AmazonUI.productStorage.getBook(bookIndex);
-				updateUI();
-			}
-		});
-		button_1.setBounds(366, 243, 75, 29);
-		getContentPane().add(button_1);
-
-		JLabel lblViewOtherProducts = new JLabel("View Other Products");
-		lblViewOtherProducts.setHorizontalAlignment(JLabel.CENTER);
-		lblViewOtherProducts.setBounds(234, 248, 145, 16);
-		getContentPane().add(lblViewOtherProducts);
-		updateUI(); 
 	}
 
 }
-
--- AmazonProject/src/ToysUI.java
+++ AmazonProject/src/ToysUI.java
@@ -30,7 +30,6 @@ public class ToysUI extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private static int bookIndex;
 
 	/**
 	 * Launch the application.
@@ -55,106 +54,9 @@ public class ToysUI extends JFrame {
 	static JLabel lblProductPhoto = new JLabel("Product Photo ");
 	static JLabel lblProductName = new JLabel(" NAME: ");
 	static JLabel lblAttributes = new JLabel("");
-<<<<<<< HEAD
-	/**
-	 * Create the frame.
-	 */
+
+	
 	public ToysUI() {
-=======
->>>>>>> branch 'master' of https://github.com/sarikap8/Amazon-Project.git
-
-<<<<<<< HEAD
-
-			setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			setBounds(100, 100, 831, 551);
-
-			Container content = getContentPane();
-			content.setBackground(new Color(204, 204, 204));
-			getContentPane().setLayout(null);
-
-			JMenuBar menuBar = new JMenuBar();
-			menuBar.setBounds(0, 0, 807, 30);
-			getContentPane().add(menuBar);
-
-			JMenuItem menuItemFiction = new JMenuItem("Dolls");
-			menuBar.add(menuItemFiction);
-
-			JMenuItem menuItemNonfiction = new JMenuItem("Trucks");
-			menuBar.add(menuItemNonfiction);
-
-			JMenuItem menuItemScifi = new JMenuItem("Legos");
-			menuBar.add(menuItemScifi);
-
-			lblProductPhoto.setBounds(6, 77, 264, 334);
-			
-			getContentPane().add(lblProductPhoto);
-			
-			lblProductName.setForeground(new Color(255, 255, 255));
-			lblProductName.setBackground(new Color(102, 0, 0));
-			lblProductName.setOpaque(true);
-			lblProductName.setBounds(10, 42, 797, 23);
-			getContentPane().add(lblProductName);
-
-			lblRating.setBackground(new Color(153, 102, 102));
-			lblRating.setOpaque(true);
-			lblRating.setBounds(276, 67, 531, 30);
-			getContentPane().add(lblRating);
-
-			JButton button = new JButton("<");
-			button.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				}
-			});
-			button.setBounds(0, 423, 75, 29);
-			getContentPane().add(button);
-
-			JButton button_1 = new JButton(">");
-			button_1.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-				}
-			});
-			
-						lblAttributes.setOpaque(true);
-						lblAttributes.setBackground(new Color(204, 153, 153));
-						lblAttributes.setBounds(276, 104, 531, 307);
-						getContentPane().add(lblAttributes);
-			button_1.setBounds(209, 423, 75, 29);
-			getContentPane().add(button_1);
-
-			JLabel lblViewOtherProducts = new JLabel("View Other Products");
-			lblViewOtherProducts.setHorizontalAlignment(JLabel.CENTER);
-			lblViewOtherProducts.setBounds(77, 428, 145, 16);
-			getContentPane().add(lblViewOtherProducts);
-			
-			JButton btnAddtoCart = new JButton("Add to cart");
-			btnAddtoCart.setBounds(671, 423, 136, 41);
-			getContentPane().add(btnAddtoCart);
-	        
-	        JLabel lblProductName = new JLabel(" NAME: ");
-	        lblProductName.setForeground(new Color(255, 255, 255));
-	        lblProductName.setBackground(new Color(102, 0, 0));
-	        lblProductName.setOpaque(true);
-	        lblProductName.setBounds(10, 42, 434, 23);
-	        getContentPane().add(lblProductName);
-		
-		
-		
-=======
-	public static void updateUI() {
-		Books b = AmazonUI.productStorage.getBook(bookIndex);
-		lblAttributes.setText("<html> Author: " + b.getAuthorName() + "<br> </br> Publisher: " + b.getPublisherName()
-				+ "<br> </br> Page Count: " + b.getPageCount() + "<br> </br>");
-		lblProductPhoto
-		.setIcon(new ImageIcon("src/" + AmazonUI.productStorage.getBook(bookIndex).getPhotoName() + ".jpg"));
-		lblRating.setText("RATING: " + AmazonUI.productStorage.getBook(bookIndex).getRating());
-		lblProductName.setText("NAME: " + AmazonUI.productStorage.getBook(bookIndex).getName()); 
->>>>>>> branch 'master' of https://github.com/sarikap8/Amazon-Project.git
-	}
-
-<<<<<<< HEAD
-=======
-	public ToysUI() {
-		bookIndex = 0;
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 831, 551);
 
@@ -169,16 +71,13 @@ public class ToysUI extends JFrame {
 		JMenuItem menuItemFiction = new JMenuItem("Dolls");
 		menuBar.add(menuItemFiction);
 
-		JMenuItem menuItemNonfiction = new JMenuItem("Trucks");
+		JMenuItem menuItemNonfiction = new JMenuItem("Legos");
 		menuBar.add(menuItemNonfiction);
 
-		JMenuItem menuItemScifi = new JMenuItem("Legos");
+		JMenuItem menuItemScifi = new JMenuItem("Cars");
 		menuBar.add(menuItemScifi);
 
-		JButton btnAddtoCart = new JButton("Add to cart");
-		menuBar.add(btnAddtoCart);
-
-		lblProductPhoto.setBounds(6, 77, 145, 174);
+		lblProductPhoto.setBounds(6, 77, 264, 334);
 		
 		getContentPane().add(lblProductPhoto);
 		
@@ -190,47 +89,40 @@ public class ToysUI extends JFrame {
 
 		lblRating.setBackground(new Color(153, 102, 102));
 		lblRating.setOpaque(true);
-		lblRating.setBounds(163, 67, 272, 30);
+		lblRating.setBounds(276, 67, 531, 30);
 		getContentPane().add(lblRating);
 
 		lblAttributes.setOpaque(true);
 		lblAttributes.setBackground(new Color(204, 153, 153));
-		lblAttributes.setBounds(163, 104, 272, 127);
+		lblAttributes.setBounds(276, 104, 531, 307);
 		getContentPane().add(lblAttributes);
 
 		JButton button = new JButton("<");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				bookIndex --; 
-				if(bookIndex < 0) {
-					bookIndex = 2; 
-				}
-				updateUI(); 
+				 
 			}
 		});
-		button.setBounds(157, 243, 75, 29);
+		button.setBounds(0, 423, 75, 29);
 		getContentPane().add(button);
 
 		JButton button_1 = new JButton(">");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				bookIndex++; 
-				if (bookIndex > 2) {
-					bookIndex = 0; 
-				}
-				//Books c = AmazonUI.productStorage.getBook(bookIndex);
-				updateUI();
+				
 			}
 		});
-		button_1.setBounds(366, 243, 75, 29);
+		button_1.setBounds(209, 423, 75, 29);
 		getContentPane().add(button_1);
 
 		JLabel lblViewOtherProducts = new JLabel("View Other Products");
 		lblViewOtherProducts.setHorizontalAlignment(JLabel.CENTER);
-		lblViewOtherProducts.setBounds(234, 248, 145, 16);
+		lblViewOtherProducts.setBounds(77, 428, 145, 16);
 		getContentPane().add(lblViewOtherProducts);
-		updateUI(); 
+		
+				JButton btnAddtoCart = new JButton("Add to cart");
+				btnAddtoCart.setBounds(671, 423, 136, 41);
+				getContentPane().add(btnAddtoCart);
 	}
 
->>>>>>> branch 'master' of https://github.com/sarikap8/Amazon-Project.git
 }
