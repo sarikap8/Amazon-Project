fdfdf837322eff9cb8f6bacee4d64e936c362143
1861657 <1861657@20.8.20.52>
2020-02-28 12:31:59 -0800

    Clothing class

--- AmazonProject/src/Clothing.java
+++ AmazonProject/src/Clothing.java
@@ -1,4 +1,22 @@
 
 public class Clothing extends Product {
-
+	private String clothingSize; 
+	private String clothingColor; 
+	private String clothingMaterial; 
+	//private String photoName; 
+	public Clothing(String size, String color, String material, String photoName, double price) {
+		this.clothingSize = size; 
+		this.clothingColor = color; 
+		this.clothingMaterial = material; 
+		this.setPhotoName(photoName); 
+	}
+	public String getClothingSize() {
+		return clothingSize;  
+	}
+	public String getClothingColor() {
+		return clothingColor; 
+	}
+	public String getClothingMaterial() {
+		return clothingMaterial; 
+	}
 }
