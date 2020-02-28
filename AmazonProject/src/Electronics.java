f52fc2040c60ca8de5828b2a030a3ace371cf586
1838760 <1838760@20.8.20.110>
2020-02-28 12:26:41 -0800

    added to electronics class

--- AmazonProject/src/Electronics.java
+++ AmazonProject/src/Electronics.java
@@ -1,4 +1,20 @@
 
 public class Electronics extends Product {
+	private String size; 
+	private String color; 
+	//private String photoName; 
+	public Electronics(String productSize, String productColor, String photoName) {
+		this.size = productSize; 
+		this.color = productColor; 
+		this.setPhotoName(photoName); 
+	}
+	public Electronics() {
+	}
+	public String getProductSize() {
+		return size;  
+	}
 
+	public String getColor() {
+		return color; 
+	}
 }
